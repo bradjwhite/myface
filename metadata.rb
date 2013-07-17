abd93e3518f13627a602e7bde45932cd16a8d35f
Bradford White <bradjwhite926@gmail.com>
2013-07-17 09:58:48 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'myface'
+maintainer       'Bradford White'
+maintainer_email 'bwhite@24hourfit.com'
+license          'All rights reserved'
+description      'Installs/Configures myface'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
